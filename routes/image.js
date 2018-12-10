b53feaa52efd7547827551f49f222ad9069e9e1d
clarisssa <clarissaparo@gmail.com>
2018-12-10 13:25:56 -0300

    delete alta images

--- routes/image.js
+++ routes/image.js
@@ -3,24 +3,6 @@ var router = require('express').Router();
 var Image = mongoose.model('image');
 
 
-//Alta de Imagenes
-router.post('/', (req, res, next) => {
-    let name = req.body.name;
-    let url = req.body.url;
-
-    var image = new Image({
-        name: name,
-        url : url,
-    });
-
-    image.save().then(function (us) {
-        res.send("Image had been posted \n" + image);
-    }, function (err) {
-        res.send("The image has not been registered correctly");
-    })
-});
-
-
 //Listar todas las Imagenes
 router.get('/', (req, res, next) => {
     
